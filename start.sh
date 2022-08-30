401b31d05d382697bb24e260d28e6f5225453547
hlzheng-git <52538471+hlzheng-git@users.noreply.github.com>
2022-08-30 18:39:53 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,19 +4,19 @@
 AUUID=5aaed9b7-7fe3-47c3-bb52-db59859ce198
 CADDYIndexPage=https://github.com/PavelDoGreat/WebGL-Fluid-Simulation/archive/master.zip
 CONFIGCADDY=https://raw.githubusercontent.com/test/test/master/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/test/test/master/etc/x.json
+CONFIGX=https://raw.githubusercontent.com/test/test/master/etc/x.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
-Xray_Newv=`wget --no-check-certificate -qO- https://api.github.com/repos/XTLS/Xray-core/tags | grep 'name' | cut -d\" -f4 | head -1 | cut -b 2-`
+X_Newv=`wget --no-check-certificate -qO- https://api.github.com/repos/XTLS/X-core/tags | grep 'name' | cut -d\" -f4 | head -1 | cut -b 2-`
 
 #PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
-wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/x.json
+wget -qO- $CONFIGX | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/x.json
 
 # storefiles
 mkdir -p /usr/share/caddy/$AUUID
-wget -P /usr/share/caddy/$AUUID https://github.com/XTLS/Xray-core/releases/download/v$Xray_Newv/Xray-linux-64.zip
+wget -P /usr/share/caddy/$AUUID https://github.com/XTLS/Xray-core/releases/download/v$X_Newv/X-linux-64.zip
 
 
 for file in $(ls /usr/share/caddy/$AUUID); do
